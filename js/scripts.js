a3307c6f20cddfd1d5f516a63cafc0a8871c9241
Ian Baum and Meredith Alcorn <ian@>
2016-08-11 09:18:58 -0400

    add thumbnails and functionality to switch button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function{
-  
+$(document).ready(function(){
+  $("#userInfo").submit(function(event) {
+    event.preventDefault();
+    var height = parseInt($("#userHeight").val());
+    var ferris = 24;
+    var coaster = 36;
+    var slingshot = 48;
+    if(height >= ferris) {
+      $("#ferrisWheel").show();
+    }if(height >= coaster) {
+      $("#rollerCoaster").show();
+    }if(height >= slingshot) {
+      $("#slingShot").show();
+    }else {
+      $("#noRide").show();
+    }
+  });
 });
